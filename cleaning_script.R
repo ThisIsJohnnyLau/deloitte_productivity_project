0bd60754236ac504ba4e32b97d51ae15bbef3f2b
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-05-21 08:35:08 +0100

    set up rmds for broad project areas

--- cleaning_script.R
+++ cleaning_script.R
@@ -7,3 +7,14 @@ europe_prod <- europe_prod %>%
     clean_names()
 
 
+G7_prod_hist <-
+    read_excel("data/International Labour Productivity - G7.xls",
+                sheet = "Table 3", skip = 4, n_max = 24) %>%
+                drop_na() %>% 
+    pivot_longer(cols = Canada : `G7 exc. UK`,
+                 names_to = "country",
+                 values_to = "GDP_per_hour_worked") %>%
+    rename("year" = "...1")
+
+    
+    
