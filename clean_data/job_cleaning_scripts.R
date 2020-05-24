438c26c7612b3643a27f75c6053e180b67f2b0d5
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-05-24 12:38:26 +0100

    wet code commit

--- clean_data/job_cleaning_scripts.R
+++ clean_data/job_cleaning_scripts.R
@@ -1,16 +1,17 @@
 #Sectors
 
-natural_resources <- c("a", "b")
+natural_res_and_util <- c("a", "b", "d", "e", "a_b_d_and_e")
 construction <- "f"
 manufacturing <- "c"
 telecoms <- "j"
 trade <- "g"
 leisure_and_entertainment <- c("i", "r")
-trans_and_util <- c("d", "e", "h")
+transport <- "h"
 ed_and_health <- c("p", "q")
 prof_and_biz_services <- c("m", "n")
 public_admin_and_def <- "o"
 financial_services <- c("k", "l")
+other_services <- c("s", "t", "s_and_t")
 
 
 
@@ -43,19 +44,20 @@ UK_Labour_Productivity_Jobs_in_Regions_by_Industry <- read_excel("raw_data/data/
     rename(year_month = sic_2007_section) %>% 
     mutate(year_month = as.Date(as.yearmon(str_sub(year_month, 0, -4), "%b %y"))) %>%
     pivot_longer(cols = a:t, names_to = "subsector_letter", values_to = "jobs") %>%
-    mutate(industry =  case_when(subsector_letter %in% natural_resources ~ "natural_resources",
+    mutate(industry =  case_when(subsector_letter %in% natural_res_and_util ~ "natural_resources_and_utilities",
                                  subsector_letter %in% construction ~ "construction",
                                  subsector_letter %in% manufacturing ~ "manufacturing",
                                  subsector_letter %in% telecoms ~ "telecoms",
                                  subsector_letter %in% trade ~ "trade",
                                  subsector_letter %in% leisure_and_entertainment ~ "leisure_and_entertainment",
-                                 subsector_letter %in% trans_and_util ~ "transport_and_utilities",
+                                 subsector_letter %in% transport ~ "transport",
                                  subsector_letter %in% ed_and_health ~ "education_and_health",
                                  subsector_letter %in% prof_and_biz_services ~ "prof_and_biz_services",
                                  subsector_letter %in% public_admin_and_def ~ "public_admin_and_defence",
                                  subsector_letter %in% financial_services ~ "financial_services",
-                                 TRUE ~ "other")
-    ) %>%
+                                 subsector_letter %in% other_services ~ "other_services",
+                                 TRUE ~ subsector_letter)
+    )  %>%
     relocate(industry, .before = subsector_letter) %>%
     drop_na() %>% 
     print()
@@ -69,18 +71,19 @@ scotland_jobs <- read_excel("raw_data/data/UK_productivity/UK Labour Productivit
     rename(year_month = sic_2007_section) %>% 
     mutate(year_month = as.Date(as.yearmon(str_sub(year_month, 0, -4), "%b %y"))) %>%
     pivot_longer(cols = a:t, names_to = "subsector_letter", values_to = "jobs") %>%
-    mutate(industry =  case_when(subsector_letter %in% natural_resources ~ "natural_resources",
+    mutate(industry =  case_when(subsector_letter %in% natural_res_and_util ~ "natural_resources_and_utilities",
                                  subsector_letter %in% construction ~ "construction",
                                  subsector_letter %in% manufacturing ~ "manufacturing",
                                  subsector_letter %in% telecoms ~ "telecoms",
                                  subsector_letter %in% trade ~ "trade",
                                  subsector_letter %in% leisure_and_entertainment ~ "leisure_and_entertainment",
-                                 subsector_letter %in% trans_and_util ~ "transport_and_utilities",
+                                 subsector_letter %in% transport ~ "transport",
                                  subsector_letter %in% ed_and_health ~ "education_and_health",
                                  subsector_letter %in% prof_and_biz_services ~ "prof_and_biz_services",
                                  subsector_letter %in% public_admin_and_def ~ "public_admin_and_defence",
                                  subsector_letter %in% financial_services ~ "financial_services",
-                                 TRUE ~ "other")
+                                 subsector_letter %in% other_services ~ "other_services",
+                                 TRUE ~ subsector_letter)
     ) %>%
     relocate(industry, .before = subsector_letter) %>%
     drop_na() %>% 
