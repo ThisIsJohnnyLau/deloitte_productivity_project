a5f867a7c1041c0f9bd74419e78ad19f7454afb9
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-05-24 20:21:33 +0100

    included all UK regions for productivity measures

--- clean_data/cleaning_script.R
+++ clean_data/cleaning_script.R
@@ -53,10 +53,105 @@ scotland_output_per_job <- read_xls("raw_data/data/UK_productivity/UK Labour Pro
 london_output_per_job <- read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls", sheet = "OpJ (value)", range = "DQ7:EG27") %>%
     clean_names()  %>%
     cbind(., years) %>% 
-    mutate(region = "Scotland") %>%
+    mutate(region = "London") %>%
     pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
 
-UK_regional_output_per_job <- rbind(UK_output_per_job, scotland_output_per_job, london_output_per_job) %>%
+NE_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "S7:AI27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "North East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+NW_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "AJ7:AZ27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "North West") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+
+yorkshire_and_humber_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "BA7:BQ27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Yorkshire and the Humber") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+
+E_midlands_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "BR7:CH27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "East Midlands") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+W_midlands_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "CI7:CY27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "West Midlands") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+E_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "CZ7:DP27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+SE_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "EH7:EX27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "South East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+SW_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "EY7:FO27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "South West") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+wales_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "FP7:GF27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Wales") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+NI_output_per_job <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpJ (value)", range = "FP7:GF27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Northern Ireland") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+
+UK_regional_output_per_job <-
+    rbind(UK_output_per_job,
+          scotland_output_per_job,
+          london_output_per_job,
+          NE_output_per_job,
+          NW_output_per_job,
+          yorkshire_and_humber_output_per_job,
+          E_midlands_output_per_job,
+          W_midlands_output_per_job,
+          E_output_per_job,
+          SE_output_per_job,
+          SW_output_per_job,
+          wales_output_per_job,
+          NI_output_per_job) %>%
     mutate(industry =  case_when(subsector_letter %in% natural_res_and_util ~ "natural_resources_and_utilities",
                                  subsector_letter %in% construction ~ "construction",
                                  subsector_letter %in% manufacturing ~ "manufacturing",
@@ -75,25 +170,124 @@ UK_regional_output_per_job <- rbind(UK_output_per_job, scotland_output_per_job, 
 
 # Output per hour
 
+years <- read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls", sheet = "OpH (value)", range = "A7:A27") %>% 
+    rename(year = ...1)
+
 UK_output_per_hour <- read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls", sheet = "OpH (value)", range = "A7:R27") %>%
     clean_names() %>%
     rename(year = x1) %>%
     mutate(region = "UK") %>% 
-    pivot_longer(cols = c(-year,-region), names_to = "subsector_letter", values_to = "output_per_job")
+    pivot_longer(cols = c(-year,-region), names_to = "subsector_letter", values_to = "output_per_hour")
 
 scotland_output_per_hour <- read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls", sheet = "OpH (value)", range = "GG7:GW27") %>%
     clean_names()  %>%
     cbind(., years) %>% 
     mutate(region = "Scotland") %>%
-    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_job")
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
 
 london_output_per_hour <- read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls", sheet = "OpH (value)", range = "DQ7:EG27") %>%
     clean_names()  %>%
     cbind(., years) %>% 
     mutate(region = "London") %>%
-    pivot_longer(cols = c(-year,-region), names_to = "subsector_letter", values_to = "output_per_job")
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
 
-UK_regional_output_per_hour <- rbind(UK_output_per_hour, scotland_output_per_hour, london_output_per_hour) %>%
+NE_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "S7:AI27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "North East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+NW_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "AJ7:AZ27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "North West") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+
+yorkshire_and_humber_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "BA7:BQ27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Yorkshire and the Humber") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+
+E_midlands_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "BR7:CH27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "East Midlands") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+W_midlands_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "CI7:CY27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "West Midlands") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+E_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "CZ7:DP27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+SE_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "EH7:EX27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "South East") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+SW_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "EY7:FO27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "South West") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+wales_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "FP7:GF27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Wales") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+NI_output_per_hour <-
+    read_xls("raw_data/data/UK_productivity/UK Labour Productivity - Region by Industry.xls",
+             sheet = "OpH (value)", range = "FP7:GF27") %>%
+    clean_names()  %>%
+    cbind(., years) %>% 
+    mutate(region = "Northern Ireland") %>%
+    pivot_longer(cols = c(-year, -region), names_to = "subsector_letter", values_to = "output_per_hour")
+
+UK_regional_output_per_hour <-
+    rbind(UK_output_per_hour,
+          scotland_output_per_hour,
+          london_output_per_hour,
+          NE_output_per_hour,
+          NW_output_per_hour,
+          yorkshire_and_humber_output_per_hour,
+          E_midlands_output_per_hour,
+          W_midlands_output_per_hour,
+          E_output_per_hour,
+          SE_output_per_hour,
+          SW_output_per_hour,
+          wales_output_per_hour,
+          NI_output_per_hour) %>%
     mutate(industry =  case_when(subsector_letter %in% natural_res_and_util ~ "natural_resources_and_utilities",
                                  subsector_letter %in% construction ~ "construction",
                                  subsector_letter %in% manufacturing ~ "manufacturing",
